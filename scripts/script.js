e8546892066c131c47631e7b6cecb5e79a8df16c
Kurt Leadley <kleadley@my365.bellevue.edu>
2019-08-30 02:22:23 -0400

    mobile

--- scripts/script.js
+++ scripts/script.js
@@ -115,4 +115,15 @@ $(function() {
     //     $(id).append("<p class='thumb-text'><a href='"+thumbObject[divId].link+"'>"+thumbObject[divId].title+"</a></p>");
     //     $(id).append("<p class='thumb-text'><a href='"+thumbObject[divId].link+"'>"+thumbObject[divId].desc+"</a></p>");
     // }
+    /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 });
+// mobile nav taken from
+// https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
